70cbc12b7e140e5b5037dafcf8d7a71f52f8fe10
LiquidVacuum <venomandroid@gmail.com>
2021-04-07 06:27:07 +0300

    feat: add 01-matrix-elements-sum solution

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,15 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let output = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (!matrix[j][i]) break;
+      output += matrix[j][i];
+    }
+  }
+  return output;
 }
 
 module.exports = getMatrixElementsSum;
