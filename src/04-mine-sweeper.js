b8cfb4896187b0647a22fd095a9a0849d403bd20
LiquidVacuum <venomandroid@gmail.com>
2021-04-08 08:24:37 +0300

    feat: add 04-mine-sweeper solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,28 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const output = matrix.slice().map((elem) => elem.slice().fill(0));
+
+  function markPerimeter(x, y) {
+    for (let k = x - 1; k <= x + 1; k++) {
+      for (let m = y - 1; m <= y + 1; m++) {
+        if (k !== x || m !== y) {
+          if (output[k] !== undefined && output[k][m] !== undefined) {
+            output[k][m] += 1;
+          }
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) markPerimeter(i, j);
+    }
+  }
+
+  return output;
 }
 
 module.exports = minesweeper;
